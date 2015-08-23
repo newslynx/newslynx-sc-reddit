adb3001fbf0de851549d8b2ba983d4e1b93d5d4f
abelsonlive <brianabelson@gmail.com>
2015-08-22 21:14:34 -0400

    test build

--- .travis.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh 
-
-mkdir ~/.newslynx/
-wget https://github.com/newslynx/newslynx-core/blob/feature/modular/newslynx/dot_newslynx/config.yaml -o ~/.newslynx/config.yaml
-cat ~/.newslynx/config.yaml
